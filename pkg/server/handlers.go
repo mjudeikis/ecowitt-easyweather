c459a82eb6dd224f1fe63b0c1478c134241b32e3
Mangirdas <mangirdas@judeikis.lt>
2022-07-02 17:00:39 +0300

    debug

--- pkg/server/handlers.go
+++ pkg/server/handlers.go
@@ -5,8 +5,13 @@ import (
 	"io/ioutil"
 	"log"
 	"net/http"
+	"reflect"
 	"strings"
 	"time"
+
+	"github.com/mjudeikis/weewx-easyweather/pkg/api"
+
+	"github.com/davecgh/go-spew/spew"
 )
 
 func (s *Server) ingest(w http.ResponseWriter, r *http.Request) {
@@ -30,11 +35,16 @@ func (s *Server) ingest(w http.ResponseWriter, r *http.Request) {
 	}
 
 	data := string(body)
+	spew.Dump(data)
+	metrics := api.WeatherData{}
 	parts := strings.Split(data, "&")
 	for _, part := range parts {
 		p := strings.Split(part, "=")
-		log.Println(p[0], p[1])
+		setField(&metrics, p[0], p[1])
 	}
+
+	spew.Dump(metrics)
+
 }
 
 func (s *Server) metrics(w http.ResponseWriter, r *http.Request) {
@@ -48,4 +58,33 @@ func (s *Server) metrics(w http.ResponseWriter, r *http.Request) {
 		fmt.Println(k, " => ", v)
 	}
 
+}
+
+func setField(item interface{}, fieldName string, value interface{}) error {
+	v := reflect.ValueOf(item).Elem()
+	if !v.CanAddr() {
+		return fmt.Errorf("cannot assign to the item passed, item must be a pointer in order to assign")
+	}
+	// It's possible we can cache this, which is why precompute all these ahead of time.
+	findJsonName := func(t reflect.StructTag) (string, error) {
+		if jt, ok := t.Lookup("json"); ok {
+			return strings.Split(jt, ",")[0], nil
+		}
+		return "", fmt.Errorf("tag provided does not define a json tag", fieldName)
+	}
+	fieldNames := map[string]int{}
+	for i := 0; i < v.NumField(); i++ {
+		typeField := v.Type().Field(i)
+		tag := typeField.Tag
+		jname, _ := findJsonName(tag)
+		fieldNames[jname] = i
+	}
+
+	fieldNum, ok := fieldNames[fieldName]
+	if !ok {
+		return fmt.Errorf("field %s does not exist within the provided item", fieldName)
+	}
+	fieldVal := v.Field(fieldNum)
+	fieldVal.Set(reflect.ValueOf(value))
+	return nil
 }
