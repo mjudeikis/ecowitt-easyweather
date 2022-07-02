9e7cea45d26fb62c686c296a947d4ab53472a9bc
Mangirdas <mangirdas@judeikis.lt>
2022-07-02 16:24:50 +0300

    add debug

--- pkg/server/handlers.go
+++ pkg/server/handlers.go
@@ -3,18 +3,20 @@ package server
 import (
 	"fmt"
 	"net/http"
+
+	"github.com/davecgh/go-spew/spew"
 )
 
 func (s *Server) ingest(w http.ResponseWriter, r *http.Request) {
 	//ctx := r.Context()
 	s.log.Info("ingest")
-
+	spew.Dump(r.URL.Query())
 	// Parse the request body into a struct.
 	//var body api.WeatherData
 
 	values := r.URL.Query()
 	for k, v := range values {
-		fmt.Println(k, " => ", v)
+		spew.Dump(k, " => ", v)
 	}
 
 }
